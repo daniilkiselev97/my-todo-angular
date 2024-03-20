826e2ef846339e17bcdb63b407012d40fbbb7e50
Daniil Kiselev <danqkiselev@yandex.ru>
2024-03-21 02:47:12 +0300

    create task-form component

---
+++ src/app/task-form/task-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { IItem, TodoListService } from '../services/todo-list.service';
+
+@Component({
+  selector: 'task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.scss'],
+})
+export class TaskFormComponent {
+  newTitle!: string;
+  constructor() {}
+
+  @Output() 
+  createItem = new EventEmitter<any>();
+  
+}
