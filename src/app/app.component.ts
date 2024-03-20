81877917e06d406d606605f6e7f0ae5df16f4007
Daniil Kiselev <danqkiselev@yandex.ru>
2024-03-21 02:54:22 +0300

    changed app-module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit,  } from '@angular/core';
+import { IItem, TodoListService } from './services/todo-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit {
+  constructor(public todoListService: TodoListService){}
+  title = 'Todo List';
+  taskList:Array<IItem> = this.todoListService.tasks
+
+ 
+
+  ngOnInit(): void {
+      // if (localStorage.getItem('my_tasks')) {
+      //   this.taskList = JSON.parse(localStorage.getItem('my_tasks')!);
+      // }
+  }
+
+
+  // onComplete(task: any) {
+  //   task.completed = true
+  //   console.log('complete', task);
+  //   this.httpService.updateTasks(task).subscribe(()=>{
+  //     this.getAllTasks()
+
+
+  //   })
+  // }
+  // onImportant(task: any) {
+  //   task.important = true
+
+  //   console.log('important', task);
+  //   this.httpService.updateTasks(task).subscribe(()=>{
+  //     this.getAllTasks()
+      
+  //   })
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TaskFormComponent } from './task-form/task-form.component';
-import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskFormComponent,
+    TaskListComponent,
+    
     TaskItemComponent,
-    TaskListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule,  FormsModule,HttpClientModule, CommonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
