8411541bc0fc8cb18af66b36b5e3a2801bfdf5ea
Daniil Kiselev <danqkiselev@yandex.ru>
2024-03-26 12:40:25 +0300

    added fonts for page-about and exec reactive-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TaskListComponent } from './task-list/task-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { TaskItemComponent } from './task-item/task-item.component';
@@ -28,7 +28,14 @@ import { MyEmptyPageComponent } from './my-empty-page/my-empty-page.component';
     TodoListComponent,
     MyEmptyPageComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule, CommonModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CommonModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/authorization/authorization.component.ts
+++ src/app/authorization/authorization.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html',
-  styleUrls: ['./authorization.component.css']
+  styleUrls: ['./authorization.component.css'],
 })
 export class AuthorizationComponent {
 
+
+  public myForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
 }
